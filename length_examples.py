423f322bb89c401857cd55ad75183ca79eaad116
shelbycox <aanford@gmail.com>
2021-09-30 14:51:33 -0400

    Update length_examples.py

--- length_examples.py
+++ length_examples.py
@@ -3,13 +3,14 @@ import tropicalLines as tl
 
 def test_1():
 	for j in range(10):
-		print(10+j, ((10+j)*(10+j-1))/2)
+		l0 = ((10+j)*(10+j-1))/2
+		print(10+j, l0)
 		for i in range(10):
 			u, v = pf.rand_pair(10+j)
 			line = tl.get_tropical_line(u,v)
 			red_line = tl.reduce_line(line)
 			l1 = len(line)
 			l2 = len(red_line)
-			print(l1, l2, l2 - l1)
+			print(l1, l2, l2 - l1, l2/l0)
 
 test_1()
