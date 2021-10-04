19f0d34101b46c6e055de14364ae9cdb58805e2c
shelbycox <aanford@gmail.com>
2021-10-04 14:10:54 -0400

    tropicalLines addition

--- length_examples.py
+++ length_examples.py
@@ -13,4 +13,32 @@ def test_1():
 			l2 = len(red_line)
 			print(l1, l2, l2 - l1, l2/l0)
 
-test_1()
+def list_finder(L, i):
+	indices = [[j] for j in range(len(L))]
+	count = 0
+	while count < 100:
+		# print(i)
+		count = count + 1
+		# print(indices)
+		new_indices = []
+		for I in indices:
+			curr = L
+			for j in I:
+				curr = curr[j]
+				# print(curr)
+			if type(curr) is int:
+				# print('true')
+				if curr == i:
+					# print(i, curr, 'true true')
+					return I
+			else:
+				for k in range(len(curr)):
+					new_I = I.copy()
+					new_I.append(k)
+					new_indices.append(new_I)
+		indices = new_indices
+
+print(list_finder([[1,2],[3,4]], 1))
+print(list_finder([[1,2],[3,4]], 2))
+print(list_finder([[1,2],[3,4]], 3))
+print(list_finder([[1,2],[3,4]], 4))
